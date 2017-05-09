87c2424d48ba72ee880cac107d7f0cddbf49cd9a
jeffreyung <jeffrey.ung97@gmail.com>
2017-05-09 13:08:39 +1000

    Update SwingUI.java

--- src/comp2911/gui/SwingUI.java
+++ src/comp2911/gui/SwingUI.java
@@ -3,13 +3,13 @@ package comp2911.gui;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JPopupMenu;
 
 import comp2911.Constants;
-import comp2911.GameEngine;
 import comp2911.gui.panel.StartPanel;
 
 /**
@@ -17,21 +17,22 @@ import comp2911.gui.panel.StartPanel;
  */
 @SuppressWarnings("serial")
 public class SwingUI extends JFrame {
-	
-	private GameEngine gameEngine;
+
+	/**
+	 * The current panel.
+	 */
 	private JPanel panel;
 	
 	/**
-	 * Constructs the SwingUI class
+	 * Constructs the SwingUI class.
 	 */
-	public SwingUI(GameEngine gameEngine) {
-		this.gameEngine = gameEngine;
+	public SwingUI() {
 		this.panel = new StartPanel(this);
 		this.init();
 	}
 	
 	/**
-	 * Initializes the JFrame
+	 * Initializes the JFrame.
 	 */
 	public void init() {
 		try {
@@ -46,31 +47,37 @@ public class SwingUI extends JFrame {
 			this.pack();
 			this.setVisible(true);
 			this.setResizable(false);
-		} catch (Exception e) {
+		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
 
 	/**
-	 * @return JPanel the current panel viewed by the user
+	 * @return JPanel the current panel viewed by the user.
 	 */
 	public JPanel getPanel() {
 		return panel;
 	}
 
 	/**
-	 * Sets the panel to be viewed by the user
-	 * @param panel to be viewed
+	 * Sets the panel to be viewed by the user.
+	 * @param panel to be viewed.
 	 */
 	public void setPanel(JPanel panel) {
 		this.panel = panel;
 	}
-	
+
 	/**
-	 * @return GameEngine the game engine
+	 * Updates the current interface of the panel.
+	 * The current panel should be the game panel.
+	 * @param board the board to be displayed on the interface.
 	 */
-	public GameEngine getGameEngine() {
-		return gameEngine;
+	public void updateInterface(ArrayList<ArrayList<Character>> board) {
+		/**
+		 * TODO
+		 * we know current panel is game panel
+		 */
+		// paint panel ?
 	}
-	
+
 }
