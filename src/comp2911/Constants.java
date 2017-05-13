b61318cd75ee14192beba2f35c45fa267c7356db
jeffreyung <jeffrey.ung97@gmail.com>
2017-05-13 21:23:28 +1000

    Update Constants.java

--- src/comp2911/Constants.java
+++ src/comp2911/Constants.java
@@ -15,4 +15,20 @@ public class Constants {
 	 */
 	public static final boolean DEBUG_MODE = true;
 	
+	/**
+	 * The board size.
+	 */
+	public static final int BOARD_SIZE = 10;
+	
+	/**
+	 * The maximum board size.
+	 * Set this to an even number so the main game board can be placed at the center.
+	 */
+	public static final int MAX_BOARD_SIZE = 10;
+	
+	/**
+	 * The tile size.
+	 */
+	public static final int TILE_SIZE = 25;
+	
 }
