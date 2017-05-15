852792bc6d65d654bb968a7c6b8135178ccf9dc4
jeffreyung <jeffrey.ung97@gmail.com>
2017-05-15 23:01:54 +1000

    Update GameEngine.java

--- src/comp2911/GameEngine.java
+++ src/comp2911/GameEngine.java
@@ -5,6 +5,7 @@ import java.util.logging.Logger;
 
 import comp2911.game.Direction;
 import comp2911.game.MapGenerator;
+import comp2911.game.Position;
 import comp2911.gui.SwingUI;
 import comp2911.gui.UserInput;
 
@@ -26,12 +27,18 @@ public class GameEngine {
 	private ArrayList<ArrayList<Character>> board;
 	
 	/**
+	 * Player's current position.
+	 */
+	private Position playerPos;
+	
+	/**
 	 * Constructs a new GameEngine and initializes the variables.
 	 */
 	public GameEngine(SwingUI swingUI) {
 		this.swingUI = swingUI;
 		this.mapGenerator = new comp2911.game.MapGenerator(Constants.BOARD_SIZE);
 		this.board = this.mapGenerator.createBoard();
+		this.playerPos = getPlayerPos();
 	}
 	
 	/**
@@ -39,25 +46,65 @@ public class GameEngine {
 	 * @param input 
 	 */
 	public void sendUserDirection(Direction dir) {
-		/**
-		 * if (dir == Direction.UP)
-		 * 		go up
-		 * if (dir == Direction.DOWN
-		 * 		go down
-		 * if (dir == Direction.LEFT
-		 * 		go left
-		 * if (dir == DIRECTION.RIGHT
-		 * 		go right
-		 * this.board = ???;
-		 */
-		/*
-		 * TODO Insert code for updating the 2D matrix here
-		 */
+		switch(dir) {
+		case UP:
+			if (getTileType(playerPos.getX(), playerPos.getY() - 1) != 'o' &&
+					getTileType(playerPos.getX(), playerPos.getY() - 1) != ' ')
+				return;
+			board.get(playerPos.getY()).set(playerPos.getX(), 'o');
+			board.get(playerPos.getY() - 1).set(playerPos.getX(), 'c');
+			playerPos.moveUp();
+			break;
+		case DOWN:
+			if (getTileType(playerPos.getX(), playerPos.getY() + 1) != 'o' &&
+					getTileType(playerPos.getX(), playerPos.getY() + 1) != ' ')
+				return;
+			board.get(playerPos.getY()).set(playerPos.getX(), 'o');
+			board.get(playerPos.getY() + 1).set(playerPos.getX(), 'c');
+			playerPos.moveDown();
+			break;
+		case LEFT:
+			if (getTileType(playerPos.getX() - 1, playerPos.getY()) != 'o' &&
+					getTileType(playerPos.getX() - 1, playerPos.getY()) != ' ')
+				return;
+			board.get(playerPos.getY()).set(playerPos.getX(), 'o');
+			board.get(playerPos.getY()).set(playerPos.getX() - 1, 'c');
+			playerPos.moveLeft();
+			break;
+		case RIGHT:
+			if (getTileType(playerPos.getX() + 1, playerPos.getY()) != 'o' &&
+					getTileType(playerPos.getX() + 1, playerPos.getY()) != ' ')
+				return;
+			board.get(playerPos.getY()).set(playerPos.getX(), 'o');
+			board.get(playerPos.getY()).set(playerPos.getX() + 1, 'c');
+			playerPos.moveRight();
+			break;
+		}
 		if(Constants.DEBUG_MODE)
-			Logger.getLogger(UserInput.class.getName()).info("Moving...");
+			Logger.getLogger(UserInput.class.getName()).info("Moving (" + playerPos.getX() + ", " + playerPos.getY() + ")");
 		this.swingUI.updateInterface();
 	}
 	
+	public char getTileType(int x, int y) {
+		return board.get(y).get(x);
+	}
+	
+	/**
+	 * @return the character's current position on the board.
+	 */
+	public Position getPlayerPos() {
+		int x = 0, y = 0;
+		for (int i = 0; i < Constants.BOARD_SIZE + 10; i++) {
+			for (int j = 0; j < Constants.BOARD_SIZE + 10; j++) {
+				if (board.get(j).get(i) == 'c') {
+					x = i;
+					y = j;
+					break;
+				}
+			}
+		}
+		return new Position(x, y);
+	}
 	/**
 	 * @return the board of the game.
 	 */
