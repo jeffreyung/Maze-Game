44113b8cb338365fb1c3cf216c0107ef331cd284
jeffreyung <jeffrey.ung97@gmail.com>
2017-05-25 22:54:36 +1000

    Delete compile.bat

--- compile.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title Build
-echo compiling...
-javac -d bin -sourcepath src src/comp2911/*.java
-javac -d bin -sourcepath src src/comp2911/game/*.java
-javac -d bin -sourcepath src src/comp2911/gui/*.java
-pause
